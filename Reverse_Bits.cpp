ce66663257d358ec12a5c342845fc5816213d42e
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-19 08:48:32 +0200

    Update Reverse_Bits.cpp

--- Reverse_Bits.cpp
+++ Reverse_Bits.cpp
@@ -32,6 +32,23 @@ public:
 return n;
     }
 };
+///////another solution /////
+class Solution2
+{
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for (int i = 0; i < 32; i++) {
+            ans =  ans<<1;
+            if(n&1){
+                ans=ans|1;
+            }
+            n = n>>1;
+        }
+        return ans;
+    }
+};
+
 Solution S1;
 int main() {
 
