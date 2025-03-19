c9e8e8ce4a69dcc67a2c8bd32c874cdc4483b76f
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-19 08:41:47 +0200

    Add files via upload

---
+++ Reverse_Bits.cpp
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        : Reverse_Bits.cpp
+// Author      : Machine Minds /Mohamed Gasser
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+
+   unsigned int arr[32]={0};
+
+ for(int i=0; i<32;i++)
+ {
+     arr[i]|=((n>>i)&1);
+
+ }
+ n=0;
+ for(int i=31,j=0;i>=0;i--)
+ {
+
+   n |=(arr[i]<<j);
+
+     j++;
+
+ }
+return n;
+    }
+};
+Solution S1;
+int main() {
+
+	int result=S1.reverseBits(30);
+	cout << result << endl; // prints !!!Hello World!!!
+	return 0;
+}
